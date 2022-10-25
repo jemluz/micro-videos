ba47a66cc7a5ba180e0a923d6501d1db748e3ab2
Jemima Luz <contatojemima@gmail.com>
2022-10-25 14:39:37 -0300

    tests and anemic entities

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,9 +1,20 @@
-import { Category } from './category';
+import { Category, CategoryProperties } from './category';
 
 describe('Category', () => {
 
-    test('constructor of category', () => {
-        const category: any = new Category('Movie');
-        expect(category.name).toBe('Movie');
+    test('constructor', () => {
+        const props: CategoryProperties = {
+            name: 'Movie',
+            description: 'Some cool category',
+            is_active: true,
+            created_at: new Date(),
+        }
+
+        const category: any = new Category(props);
+
+        expect(category.name).toBe(props.name);
+        expect(category.description).toBe(props.description);
+        expect(category.is_active).toBe(props.is_active);
+        expect(category.created_at).toBe(props.created_at);
     })
 });
